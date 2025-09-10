4f273732ba21c91d171aa6e952d072383ce27913
abhi789837 <abhishek.chauhan@oyelabs.com>
2025-09-10 18:24:30 +0530

    innitial commit

--- src/app/dashboardsubagent/subagent/notifications/page.js
+++ src/app/dashboardsubagent/subagent/notifications/page.js
@@ -1,26 +1,30 @@
 'use client'
-import Heading from "../../components/minicomponents/Heading"
-import Navbar from "../../components/minicomponents/Navbar"
-import NotificationsTabs from "./components/NotificationsTabs"
+
+import { motion } from 'framer-motion'
+import Heading from '../../components/minicomponents/Heading'
+import Navbar from '../../components/minicomponents/Navbar'
+import NotificationsTabs from './components/NotificationsTabs'
 
 export default function NotificationsPage() {
   return (
-   <motion.div
+    <motion.div
       className="mt-0 w-full flex flex-col overflow-x-hidden"
-      initial={{ opacity: 0, y: 0 }} // start hidden + slightly down
-      animate={{ opacity: 1, y: 0 }} // fade in + move up
-      transition={{ duration: 0.6, ease: 'easeInOut' }} // 400ms
+      initial={{ opacity: 0, y: 0 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.6, ease: 'easeInOut' }}
     >
       <Navbar />
 
-              <div className="ml-10 mt-6 h-10 flex pr-10 justify-between">
-                <Heading title="Notifications" subtitle={'Stay updated on new leads, contract changes, and messages'}/>
-              </div>
+      <div className="ml-10 mt-6 h-10 flex pr-10 justify-between">
+        <Heading
+          title="Notifications"
+          subtitle="Stay updated on new leads, contract changes, and messages"
+        />
+      </div>
 
-              <div className="mt-4">
-              <NotificationsTabs/>
-              </div>
-              </motion.div>
-
+      <div className="mt-4">
+        <NotificationsTabs />
+      </div>
+    </motion.div>
   )
 }
